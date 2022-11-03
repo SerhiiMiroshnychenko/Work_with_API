051de03ed8cf2e353fa1a7e27f4fdb25e6dddbb4
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-11-03 18:27:10 +0200

    Most-Starred Python Projects on GitHub. Hover texts were added.

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -19,16 +19,22 @@ response_dict = r.json()
 repo_dicts = response_dict['items']  # Значення пов'язане з 'items' - це список,
 # що містить низку словників, кожен з яких зберігає данні що до одного окремого сховища Python
 # Зберігаємо цей список словників в repo_dicts
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']  # Власник
+    description = repo_dict['description']  # Опис
+    label = f"{owner}<br />{description}"  # Формуємо інформацію
+    labels.append(label)
 
 # Зробити візуалізацію
 data = [{
     'type': 'bar',  # Тип діаграми: стовпчикова
     'x': repo_names,  # Вісь х: назви репозиторіїв
     'y': stars,  # Вісь у: зірочки
+    'hovertext': labels,  # Підказки
     'marker': {
         'color': 'rgb(60, 100, 150)',  # Блакитний колір стовпчиків
         'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}  # Сірий колір завтовшки 1,5 пікселей
